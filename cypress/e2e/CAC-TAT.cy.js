9c9b1e0594bb798d2f678b32713e52498581ea10
Eduardo-Manoel <eduardomanoelnn@yahoo.com.br>
2022-06-20 15:06:20 -0300

    Update CAC-TAT.cy.js

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -75,7 +75,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .click ()
       cy.get('.error').should('be.visible')
     })
-    it.only('envia o formuário com sucesso usando um comando customizado',function(){
+    it('envia o formuário com sucesso usando um comando customizado',function(){
       cy.fillMandatoryFieldsAndSubmit()
       cy.get('.success').should ('be.visible')
     })
