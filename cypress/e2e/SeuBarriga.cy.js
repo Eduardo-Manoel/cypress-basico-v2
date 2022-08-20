0a82fb1e56b7f2ce75472bf99b2968240b95e8aa
Eduardo-Manoel <eduardomanoelnn@yahoo.com.br>
2022-08-20 20:14:41 -0300

    main

--- cypress/e2e/SeuBarriga.cy.js
+++ cypress/e2e/SeuBarriga.cy.js
@@ -8,7 +8,6 @@ describe('SEU BARRIGA', function () {
         cy.title()
             .should('be.equal', 'Seu Barriga - Log in')
     })
-
     it('Verifica o Titulo de baixo', function () {
         cy.get('a:contains(Seu Barriga)')
             .should('be.visible')
